d39bbfe7afb22c5f4c21211b7f29a27ab5c9b20a
LolaEnBeta <lolitaenbeta@gmail.com>
2019-05-29 14:48:53 +0200

    Create index route

--- project-api.py
+++ project-api.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "Welcome to your 'Tasks for Projects'."
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
