45e36d27304bc55f1135124cd055b79305ffd062
LolaEnBeta <lolitaenbeta@gmail.com>
2019-05-29 14:54:01 +0200

    Create a route to get all projects

--- project-api.py
+++ project-api.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+projects = [
+    {"Project 1": "task 1"},
+    {"Project 2": "task 2"},
+    {"Project 3": "task 3"}
+]
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Welcome to your 'Tasks for Projects'."
+
+@app.route("/projects", methods=["GET"])
+def get_projects():
+    return jsonify({"PROJECTS": projects})
 
 if __name__ == "__main__":
     app.run(debug=True)
